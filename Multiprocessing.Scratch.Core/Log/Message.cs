6e4fb714d5c24f588c083da0614ffdece183b19a
Chris Grubb <cgrubb86@gmail.com>
2015-09-16 00:07:30 -0500

    Added core project

---
+++ Multiprocessing.Scratch.Core/Log/Message.cs
@@ -0,0 +1,21 @@
+﻿namespace Multiprocessing.Scratch.Core.Log
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class Message
+  {
+   /// <summary>
+   /// Gets or sets log message
+   /// </summary>
+    public string LogMessage { get; set; }
+
+    /// <summary>
+    /// Gets or sets the created time
+    /// </summary>
+    public DateTime? Created { get; set; }
+  }
+}
